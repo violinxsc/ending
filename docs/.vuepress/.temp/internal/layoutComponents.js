12549702bb1eb4417389aa523cc9a7dc724dc1ea
xiashoucong <xiashoucong@searchxyz.com.cn>
2022-06-27 17:55:28 +0800

    删除一些临时文件

--- docs/.vuepress/.temp/internal/layoutComponents.js
+++
@@ -1,8 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const layoutComponents = {
-  "404": defineAsyncComponent(() => import("D:/vscode/workspace/vuepress-hope/node_modules/vuepress-theme-hope/lib/client/layouts/404.js")),
-  "Layout": defineAsyncComponent(() => import("D:/vscode/workspace/vuepress-hope/node_modules/vuepress-theme-hope/lib/client/layouts/Layout.js")),
-  "Slide": defineAsyncComponent(() => import("D:/vscode/workspace/vuepress-hope/node_modules/vuepress-theme-hope/lib/client/layouts/Slide.js")),
-  "Blog": defineAsyncComponent(() => import("D:/vscode/workspace/vuepress-hope/node_modules/vuepress-theme-hope/lib/client/module/blog/layouts/Blog.js")),
-}
--- docs/.vuepress/.temp/pages/posts/index.html.vue
+++
@@ -1,3 +0,0 @@
-<template><div><h1 id="三生万物" tabindex="-1"><a class="header-anchor" href="#三生万物" aria-hidden="true">#</a> 三生万物</h1>
-<p>asasa</p>
-</div></template>
