82f519e66a86f5704889dfb0b1081206a2389657
xiashoucong <xiashoucong@searchxyz.com.cn>
2022-06-28 09:15:13 +0800

    提交

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,19 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme";
+
+export default defineUserConfig({
+  base: "/",
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "终",
+      description: "终",
+    },
+    "/en/": {
+      lang: "en-US",
+      title: "Ending",
+      description: "Ending",
+    },
+  },
+  theme,
+});
---
+++ docs/zh/work/README.md
@@ -0,0 +1,10 @@
+---
+title: 经验之谈
+index: false
+icon: creative
+category:
+  - 经验之谈
+---
+
+## some exp during working
+1111
